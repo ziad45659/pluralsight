373bcd1d6df80f8e76fc19715846a597ed26fd86
ziad.nasser <ziad400@gmail.com>
2018-01-07 04:39:00 +0100

    yo setup

---
+++ creating-apps-angular-node-token-authentication/pc_jwt/app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pcJwtApp
+ * @description
+ * # pcJwtApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pcJwtApp', []);
---
+++ creating-apps-angular-node-token-authentication/pc_jwt/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pcJwtApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pcJwtApp
+ */
+angular.module('pcJwtApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
